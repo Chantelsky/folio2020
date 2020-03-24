4415044bea6eda8a11650025994dc235ed3fc71d
Chantelsky <chantel.sky@outlook.com>
2020-03-25 03:07:46 +1000

    landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,31 @@
 import React from "react"
 import { graphql } from 'gatsby'
 import SEO from "../components/seo"
-// import styles from "../components/index.module.scss"
-// import Img from 'gatsby-image'
+import style from "../components/index.module.scss"
+import Img from 'gatsby-image'
 // import AniLink from "gatsby-plugin-transition-link/AniLink"
 
-// import { Location } from "@reach/router"
+import "../components/layout.css"
 import Header from "../components/Header"
+import Landing from "../components/Landing"
 import SocialLinks from "../components/SocialLinks"
 import Footer from "../components/Footer"
-import "../components/layout.css"
 
-const IndexPage = (props) => (
-  <main>
-    <SEO title="Home" />
-    <Header path={props.location.pathname}/>
-    <SocialLinks />
-    <Footer />
-  </main>
-)
+const IndexPage = (props) => {
+  return (
+    <main>
+      <SEO title="Home" />
+      <Header path={props.location.pathname}/>
+      <div className={style.box}></div>
+      <div className={style.image}>
+        <Img fixed ={props.data.flower.childImageSharp.fixed} />
+        </div>
+      <Landing />
+      <SocialLinks />
+      <Footer />
+    </main>
+  )
+}
 
 export default IndexPage
 
@@ -36,9 +43,9 @@ query {
     bookmark: file(relativePath: {eq: "bookmark.jpg"}) {
       ...projectImage
     }
-    chantel: file(relativePath: {eq: "chantel.jpg"}) {
+    flower: file(relativePath: {eq: "flower2.jpg"}) {
       childImageSharp{
-        fixed(width: 600, height:800){
+        fixed(width: 900, height:600){
           ...GatsbyImageSharpFixed
         }
       }
